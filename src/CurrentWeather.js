48836a849dbeb82c481a56070ded5873a738cf85
atalley3 <ashley.l.talley@gmail.com>
2022-05-07 21:49:44 -0400

    Create Weather.js
    Search form to make API call created.
    API response is passed as props to the CurrentWeather component.
    CurrentWeather is acting as placeholder.

---
+++ src/CurrentWeather.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function CurrentWeather(props) {
+  return (
+    <div className="Current Weather">
+      <image
+        src="http://openweathermap.org/img/wn/10d@2x.png"
+        alt="rainy"
+        id="icon"
+      />
+      <div className="temp">
+        52º<span>F</span>
+      </div>
+      <div className="Details">
+        <ul>
+          <li>Humidity: 80%</li>
+          <li>Wind: 1 MPH</li>
+        </ul>
+      </div>
+      <div className="Description">
+        <div className="cityName">Asheville</div>
+        <div className="DayTime">
+          <small>Last Updated:</small>Monday 01:57PM
+        </div>
+        <div>Rainy</div>
+      </div>
+    </div>
+  );
+}
+export default CurrentWeather();
---
+++ src/Weather.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import axios from "axios";
+import CurrentWeather from "./CurrentWeather";
+
+function Weather() {
+  let [city, setCity] = useState(null);
+  let [weatherInfo, setWeatherInfo] = useState({});
+  const apiKey = "bcdada43905d3c2d7aa9f45a7ce30f8b";
+  let units = "imperial";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  function handleResponse(response) {
+    setWeatherInfo({
+      Temperature: response.data.main.temp,
+      icon: response.data.weather[0].icon,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      dt: response.data.dt,
+      city: response.data.name,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="Weather">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          className="form-control"
+          autocomplete="off"
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <CurrentWeather info={weatherInfo} />
+    </div>
+  );
+}
+
+export default Weather;
